6a176342b0e1df9aa869c6cb1cf69c893ae2274a
LeeVaughn <dlvprogramming@gmail.com>
2020-01-24 14:05:56 -0800

    created middleware to authenticate user and added it to /users GET route

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 const nameValidator = check('name')
   .exists({ checkNull: true, checkFalsy: true })
   .withMessage('Please provide a value for "name"');
+const auth = require('basic-auth');
 const bcryptjs = require('bcryptjs');
 
 // this array is used to keep track of user records as they are created
@@ -14,6 +15,57 @@ const users = [];
 
 // construct a router instance
 const router = express.Router();
+
+const authenticateUser = (req, res, next) => {
+  let message = null;
+  // parse the user's credentials from the Authorization header
+  const credentials = auth(req);
+
+  // if the user's credentials are available...
+  if (credentials) {
+    // attempt to retrieve the user from the data store by their username (i.e. the user's "key" from the Authorization header)
+    const user = users.find(u => u.username === credentials.name);
+
+    // If a user was successfully retrieved from the data store...
+    if (user) {
+      // use bcryptjs npm package to compare the user's password (from the Authorization header) to the user's password that was retrieved from the data store.
+      const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
+
+      // If the passwords match...
+      if (authenticated) {
+        console.log(`Authentication successful for username: ${user.username}`);
+
+        // then store the retrieved user object on the req object so any middleware functions that follow this middleware function will have access to the user's information.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.username}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  // If user authentication failed...
+  if (message) {
+    console.warn(message);
+
+    // return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    // Or if user authentication succeeded...
+    // Call the next() method.
+    next();
+  }
+};
+
+// route that returns the current authenticated user
+router.get('/users', authenticateUser, (req, res) => {
+  const user = req.currentUser;
+
+  res.json({ name: user.name, username: user.username });
+});
 
 // route that creates a new user
 router.post('/users', nameValidator, (req, res) => {
