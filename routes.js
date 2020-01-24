1af861592a5348aba63ebcd6ecfb236bd073382b
LeeVaughn <dlvprogramming@gmail.com>
2020-01-24 10:55:16 -0800

    required express-validator and added validation to /users POST route

--- routes.js
+++ routes.js
@@ -1,6 +1,12 @@
+/* eslint-disable linebreak-style */
+
 'use strict';
 
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+const nameValidator = check('name')
+  .exists({ checkNull: true, checkFalsy: true })
+  .withMessage('Please provide a value for "name"');
 
 // this array is used to keep track of user records as they are created
 const users = [];
@@ -9,7 +15,19 @@ const users = [];
 const router = express.Router();
 
 // route that creates a new user
-router.post('/users', (req, res) => {
+router.post('/users', nameValidator, (req, res) => {
+  // attempt to get the validation result from the request object
+  const errors = validationResult(req);
+
+  // if there are validation errors
+  if (!errors.isEmpty()) {
+    // use the array map method to get a list of error messages
+    const errorMessages = errors.array().map(error => error.msg);
+
+    // return the validation errors to the client
+    return res.status(400).json({ errors: errorMessages });
+  }
+
   // get the user from the request body
   const user = req.body;
 
